2c960ce025b1cc334d769926646643d2d7188f7a
rostgameboy <92036191+rostgameboy@users.noreply.github.com>
2023-05-24 20:18:30 +0200

    Add files via upload

---
+++ Preloader.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => { /* Страница загружена, включая все ресурсы */
+
+    const preloader = document.querySelector('.preloader') /* находим блок Preloader */
+
+    preloader.classList.add('preloader_hidden') /* добавляем ему класс для скрытия */
+})
